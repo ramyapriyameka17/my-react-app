84c1e8d1e3f799b9d03eff07d1585f250f6826e9
ramyapriyameka17 <ramyapriyameka17@gmail.com>
2025-10-23 14:02:14 -0500

    chore: add src\App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>
+          This is a simple React application created using Create React App.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
